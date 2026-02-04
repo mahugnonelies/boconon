981b34406d679cfccee01c5107225379a657111f
mahugnonelies <227068391+mahugnonelies@users.noreply.github.com>
2026-02-04 11:41:10 +0100

    Implement scroll animation for fade-up elements
    Add scroll animation for fade-up elements on scroll.

--- js/main.js
+++ js/main.js
@@ -30,3 +30,17 @@ document.getElementById("prev").onclick = () => {
   showSlide(index);
 };
 
+// SCROLL ANIMATION
+const fadeElements = document.querySelectorAll(".fade-up");
+
+window.addEventListener("scroll", () => {
+  fadeElements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100) {
+      el.classList.add("visible");
+    }
+  });
+});
+
